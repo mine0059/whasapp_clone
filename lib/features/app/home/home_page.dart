a0a375bcbad7faef7c1ecd46a10322d36be74539
mine0059 <onojefemuemine@gmail.com>
2025-07-01 14:31:24 +0100

    home and setting page UI

--- lib/features/app/home/home_page.dart
+++ lib/features/app/home/home_page.dart
@@ -1,15 +1,240 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/app/const/page_const.dart';
+import 'package:whatsapp_clone/features/app/theme/styles.dart';
+import 'package:whatsapp_clone/features/call/presentation/pages/call_contact_page.dart';
+import 'package:whatsapp_clone/features/call/presentation/pages/call_history_page.dart';
+import 'package:whatsapp_clone/features/chat/presentation/pages/chat_page.dart';
+import 'package:whatsapp_clone/features/status/presentation/pages/status_page.dart';
+import 'package:whatsapp_clone/features/user/domain/entities/user_entity.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({super.key, required this.uid, this.index});
+
+  final String uid;
+  final int? index;
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin, WidgetsBindingObserver {
+  TabController? _tabController;
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _tabController = TabController(length: 3, vsync: this);
+
+    _tabController!.addListener(() {
+      setState(() {
+        _currentIndex = _tabController!.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _tabController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'WhatsApp',
+          style: TextStyle(
+            fontSize: 20,
+            color: greyColor,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(
+                  Icons.camera_alt_outlined,
+                  color: greyColor,
+                  size: 28,
+                ),
+                onPressed: () {
+                  // Implement search functionality
+                },
+              ),
+              const SizedBox(width: 10),
+              IconButton(
+                icon: const Icon(
+                  Icons.search,
+                  color: greyColor,
+                  size: 28,
+                ),
+                onPressed: () {
+                  // Implement search functionality
+                },
+              ),
+              PopupMenuButton(
+                icon: const Icon(
+                  Icons.more_vert,
+                  color: greyColor,
+                  size: 28,
+                ),
+                color: appBarColor,
+                iconSize: 28,
+                onSelected: (value) {},
+                itemBuilder: (context) => [
+                  PopupMenuItem(
+                    value: 'settings',
+                    child: GestureDetector(
+                      onTap: () => Navigator.pushNamed(
+                          context, PageConst.settingsPage,
+                          arguments: ''),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.settings, color: tabColor),
+                          SizedBox(width: 10),
+                          Text('Settings'),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 'new_group',
+                    child: Row(
+                      children: [
+                        Icon(Icons.group, color: tabColor),
+                        SizedBox(width: 10),
+                        Text('New Group'),
+                      ],
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 'new_broadcast',
+                    child: Row(
+                      children: [
+                        Icon(Icons.broadcast_on_home, color: tabColor),
+                        SizedBox(width: 10),
+                        Text('New Broadcast'),
+                      ],
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 'linked_devices',
+                    child: Row(
+                      children: [
+                        Icon(Icons.devices, color: tabColor),
+                        SizedBox(width: 10),
+                        Text('Linked Devices'),
+                      ],
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 'starred_messages',
+                    child: Row(
+                      children: [
+                        Icon(Icons.star, color: tabColor),
+                        SizedBox(width: 10),
+                        Text('Starred Messages'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+        ],
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: tabColor,
+          labelColor: tabColor,
+          unselectedLabelColor: greyColor,
+          tabs: const [
+            Tab(text: 'Chats'),
+            Tab(text: 'Status'),
+            Tab(text: 'Calls'),
+          ],
+        ),
+      ),
+      floatingActionButton: switchFloatingActionButtonOnTapIndex(
+          _currentIndex,
+          const UserEntity(
+            uid: '1',
+            username: 'Current User',
+            phoneNumber: '1234567890',
+            profileUrl: '',
+          )),
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          Center(child: ChatPage()),
+          Center(child: StatusPage()),
+          Center(
+            child: CallHistoryPage(),
+          )
+        ],
+      ),
+    );
+  }
+
+  switchFloatingActionButtonOnTapIndex(int index, UserEntity currentUser) {
+    switch (index) {
+      case 0:
+        {
+          return FloatingActionButton(
+            backgroundColor: tabColor,
+            onPressed: () {
+              Navigator.pushNamed(context, PageConst.contactUsersPage,
+                  arguments: '');
+            },
+            child: const Icon(
+              Icons.chat,
+              color: Colors.white,
+            ),
+          );
+        }
+      case 1:
+        {
+          return FloatingActionButton(
+            backgroundColor: tabColor,
+            onPressed: () {
+              // Navigator.push(context, MaterialPageRoute(builder: (context) => const StatusPage()));
+            },
+            child: const Icon(
+              Icons.camera_alt_outlined,
+              color: Colors.white,
+            ),
+          );
+        }
+      case 2:
+        {
+          return FloatingActionButton(
+            backgroundColor: tabColor,
+            onPressed: () {
+              Navigator.pushNamed(context, PageConst.callContactPage,
+                  arguments: '');
+            },
+            child: const Icon(
+              Icons.add_call,
+              color: Colors.white,
+            ),
+          );
+        }
+      default:
+        {
+          return FloatingActionButton(
+            backgroundColor: tabColor,
+            onPressed: () {},
+            child: const Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+          );
+        }
+    }
   }
 }
---
+++ lib/features/app/settings/settings_page.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/features/app/global/widgets/profile_widget.dart';
+import 'package:whatsapp_clone/features/app/theme/styles.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key, required this.uid});
+
+  final String uid;
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              color: greyColor,
+              size: 28,
+            ),
+            onPressed: () {
+              // Implement search functionality
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          InkWell(
+            onTap: () {
+              debugPrint("Profile Just got Clicked");
+            },
+            borderRadius: BorderRadius.circular(8),
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: profileWidget(),
+                  ),
+                  const SizedBox(width: 10),
+                  const Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "username",
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          "status",
+                          style: TextStyle(color: greyColor),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.qr_code_sharp,
+                          color: tabColor,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_circle_outline,
+                          color: tabColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(),
+          const SizedBox(height: 10),
+          _settingsItemWidget(
+            title: "Account",
+            description: "Security applications, change number",
+            icon: Icons.key,
+            onTap: () {},
+          ),
+          _settingsItemWidget(
+            title: "Privacy",
+            description: "Block contacts, disappearing messages",
+            icon: Icons.lock,
+            onTap: () {},
+          ),
+          _settingsItemWidget(
+            title: "Chats",
+            description: "Theme, wallpapers, chat history",
+            icon: Icons.message,
+            onTap: () {},
+          ),
+          _settingsItemWidget(
+            title: "Logout",
+            description: "Logout, from WhatsApp Clone",
+            icon: Icons.exit_to_app,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  _settingsItemWidget(
+      {String? title,
+      String? description,
+      IconData? icon,
+      VoidCallback? onTap}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Row(
+        children: [
+          SizedBox(
+            width: 80,
+            height: 80,
+            child: Icon(
+              icon,
+              color: greyColor,
+              size: 25,
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "$title",
+                  style: const TextStyle(fontSize: 17),
+                ),
+                const SizedBox(height: 3),
+                Text(
+                  "$description",
+                  style: const TextStyle(color: greyColor),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
