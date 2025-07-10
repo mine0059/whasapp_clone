acb5801dadeaae4fc2698731a828133004f3b2c2
mine0059 <onojefemuemine@gmail.com>
2025-07-10 18:55:06 +0100

    added the upload profile usecase and also the method to upload profile to our credential cubit

--- lib/features/user/presentation/cubit/credential/credential_cubit.dart
+++ lib/features/user/presentation/cubit/credential/credential_cubit.dart
@@ -6,6 +6,7 @@ import 'package:whatsapp_clone/features/user/domain/entities/user_entity.dart';
 import 'package:whatsapp_clone/features/user/domain/usecases/credential/sign_in_with_phone_number_usecase.dart';
 import 'package:whatsapp_clone/features/user/domain/usecases/credential/verify_phone_number_usecase.dart';
 import 'package:whatsapp_clone/features/user/domain/usecases/user/create_user_usecase.dart';
+import 'package:whatsapp_clone/features/user/domain/usecases/user/upload_profile_image_usecase.dart';
 
 part 'credential_state.dart';
 
@@ -13,10 +14,13 @@ class CredentialCubit extends Cubit<CredentialState> {
   final SignInWithPhoneNumberUsecase signInWithPhoneNumberUsecase;
   final VerifyPhoneNumberUsecase verifyPhoneNumberUsecase;
   final CreateUserUsecase createUserUsecase;
+  final UploadProfileImageUsecase uploadProfileImageUsecase;
+  
   CredentialCubit({
     required this.signInWithPhoneNumberUsecase,
     required this.verifyPhoneNumberUsecase,
     required this.createUserUsecase,
+    required this.uploadProfileImageUsecase,
   }) : super(CredentialInitial());
 
   Future<void> submitVerifyPhoneNumber({
@@ -51,6 +55,20 @@ class CredentialCubit extends Cubit<CredentialState> {
       emit(CredentialFailure());
     } catch (_) {
       emit(CredentialFailure());
+    }
+  }
+
+  Future<String> uploadProfileImage({
+    required File file,
+    Function(bool isUploading)? onComplete,
+  }) async {
+    try {
+      return await uploadProfileImageUsecase.call(
+        file: file,
+        onComplete: onComplete,
+      );
+    } catch (e) {
+      throw Exception('Failed to upload profile image: $e');
     }
   }
 }
