a137b5261202edc2fbb9203b098637b0c92ea8b9
mine0059 <onojefemuemine@gmail.com>
2025-08-02 11:54:23 +0100

    added the Loading State and debugPrint

--- lib/features/user/presentation/cubit/get_device_number/get_device_number_cubit.dart
+++ lib/features/user/presentation/cubit/get_device_number/get_device_number_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/features/user/domain/entities/contact_entity.dart';
 import 'package:whatsapp_clone/features/user/domain/usecases/user/get_device_number_usecase.dart';
 
@@ -11,10 +12,20 @@ class GetDeviceNumberCubit extends Cubit<GetDeviceNumberState> {
       : super(GetDeviceNumberInitial());
 
   Future<void> getDeviceNumber() async {
+    emit(GetDeviceNumberLoading());
+    debugPrint("📱 Starting to fetch device contacts...");
+
     try {
+      final startTime = DateTime.now();
       final contactNumbers = await getDeviceNumberUsecase.call();
+      final endTime = DateTime.now();
+      final duration = endTime.difference(startTime);
+
+      debugPrint(
+          "✅ Fetched ${contactNumbers.length} contacts in ${duration.inMilliseconds}ms");
       emit(GetDeviceNumberLoaded(contacts: contactNumbers));
-    } catch (_) {
+    } catch (e) {
+      debugPrint("❌ Error fetching device contacts: $e");
       emit(GetDeviceNumberFailure());
     }
   }
