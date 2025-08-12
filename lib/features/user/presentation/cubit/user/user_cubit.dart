0f3cfd26c9db67c4c2ff534bbed3ff66b0f74e65
mine0059 <onojefemuemine@gmail.com>
2025-08-12 10:34:17 +0100

    added debug print

--- lib/features/user/presentation/cubit/user/user_cubit.dart
+++ lib/features/user/presentation/cubit/user/user_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/features/user/domain/entities/user_entity.dart';
 import 'package:whatsapp_clone/features/user/domain/usecases/user/get_all_users_usecase.dart';
 import 'package:whatsapp_clone/features/user/domain/usecases/user/update_user_usecase.dart';
@@ -14,10 +15,21 @@ class UserCubit extends Cubit<UserState> {
 
   Future<void> getAllUsers() async {
     emit(UserLoading());
-    final streamResponse = getAllUsersUsecase.call();
-    streamResponse.listen((users) {
-      emit(UserLoaded(users: users));
-    });
+    debugPrint("🔍 UserCubit: Starting to get all users");
+
+    try {
+      final streamResponse = getAllUsersUsecase.call();
+      streamResponse.listen((users) {
+        debugPrint("✅ UserCubit: Successfully loaded ${users.length} users");
+        emit(UserLoaded(users: users));
+      }, onError: (error) {
+        debugPrint("❌ UserCubit: Error loading users: $error");
+        emit(UserFailure());
+      });
+    } catch (e) {
+      debugPrint("❌ UserCubit: Exception in getAllUsers: $e");
+      emit(UserFailure());
+    }
   }
 
   Future<void> updateUser({required UserEntity user}) async {
